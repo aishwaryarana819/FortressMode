ae93be6e23d0c63139f3bfbc1f60e4efe23e65ad
Aishwarya <aishwaryarana2807@gmail.com>
2026-01-26 16:15:15 +0530

    Initial Commit

---
+++ FortressMode/FortressModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortressModeApp.swift
+//  FortressMode
+//
+//  Created by Aishwarya Rana on 26/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FortressModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
